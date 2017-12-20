bef961fb592d07c0c94451735015a1912b166249
guosheng <1535233734@qq.com>
2017-12-20 10:29:40 +0800

    【nginx反向代理】版本2.0 --- 部署

--- server.js
+++ server.js
@@ -24,67 +24,60 @@ var data = [
 
 /* ------------------------------------- 以下为处理事件 ------------------------------------- */
 
-var _methods = {
-  data: {
-    now: 0,
-    sum: 0,
-    log: ''
-  },
-  _nodeInit: function (data) { /*初始化*/
-    this._nodeFor(data)
-  },
-  _nodeFor: function (data) {
-    var self = this
-    for (var i = 0; i < data.length; i++) {
-      (function (data) {
-        /*conf/vhost/为文件默认创建地方*/
-        var mkDir = {
-          name: data.name + '.conf',
-          val: '#'+ data.name +'\n'+
-          'server{\n'+
-            'listen  '+ data.port +';\n'+
-            'server_name 127.0.0.1 localhost;\n'+
-            'access_log  logs/'+ data.name +'.log;\n'+
-            'charset utf-8;\n'+
-            'location =/ {\n'+
-              'rewrite / /'+ data.index +'.html permanent;\n'+
-            '}\n'+
-            'location / {\n'+
-              'proxy_pass  '+ data.url +';\n'+
-              'proxy_redirect  default;\n'+
-            '}\n'+
-            'location ~* ^.+\.(ico|gif|jpg|jpeg|png|html)$ {\n'+
-              'root  '+ data.path +';\n'+
-            '}\n'+
-            'location ~* ^.+\.(css|js|txt|xml|swf|wav|json)$ {\n'+
-              'root  '+ data.path +';\n'+
-            '}\n'+
-          '}\n'
-        }
-        fs.readFile('conf/vhost/' + data.name + '.conf', 'utf8', function (err) {
-          if (!err) { // 如果有该文件则先删除
-            fs.unlinkSync('conf/vhost/' + data.name + '.conf', mkDir.val, 'utf8', function (err) {
-              if (err) {
-                return console.error(err)
-              }
-            })
-            fs.appendFile('conf/vhost/' + data.name + '.conf', mkDir.val, 'utf8', function (err) {
-              if (err) {
-                return console.error(err)
-              }
-            })
-            return
-          }
-          fs.appendFile('conf/vhost/' + data.name + '.conf', mkDir.val, 'utf8', function (err) {
-            if (err) {
-              return console.error(err)
-            }
-          })
+var _nodeInit = function (data) { /*初始化*/
+  var sum = 0
+  var pathImg = 'conf/vhost/'
+  files = fs.readdirSync(pathImg)
+  files.forEach(function (file, index) { // 记总共步数
+    sum = index
+  })
+  if (fs.existsSync(pathImg)) { // 先清空内部所有配置文件
+    files.forEach(function (file, index) {
+      var curPath = pathImg + "/" + file
+      if (fs.statSync(curPath).isDirectory()) {
+        fs.rmdirSync(pathImg)
+      } else {
+        fs.unlinkSync(curPath,function (err) {
+          if (err) throw err
         })
-      })(data[i])
-    }
+      }
+      if (index === sum) { // 结束时添加
+        for (var i = 0; i < data.length; i++) {
+          (function (data) {
+            /*conf/vhost/为文件默认创建地方*/
+            var mkDir = {
+              name: data.name + '.conf',
+              val: '#'+ data.name +'\n'+
+              'server{\n'+
+                'listen  '+ data.port +';\n'+
+                'server_name 127.0.0.1 localhost;\n'+
+                'access_log  logs/'+ data.name +'.log;\n'+
+                'charset utf-8;\n'+
+                'location =/ {\n'+
+                  'rewrite / /'+ data.index +'.html permanent;\n'+
+                '}\n'+
+                'location / {\n'+
+                  'proxy_pass  '+ data.url +';\n'+
+                  'proxy_redirect  default;\n'+
+                '}\n'+
+                'location ~* ^.+\.(ico|gif|jpg|jpeg|png|html)$ {\n'+
+                  'root  '+ data.path +';\n'+
+                '}\n'+
+                'location ~* ^.+\.(css|js|txt|xml|swf|wav|json)$ {\n'+
+                  'root  '+ data.path +';\n'+
+                '}\n'+
+              '}\n'
+            }
+            fs.appendFile(pathImg + data.name + '.conf', mkDir.val, 'utf8', function (err) {
+              if (err) {
+                return console.error(err)
+              }
+            })
+          })(data[i])
+        }
+      }
+    })
   }
 }
-
 var fs = require("fs")
-_methods._nodeInit(data)
+_nodeInit(data)
